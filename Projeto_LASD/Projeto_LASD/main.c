4fa2512c838d1031b6a198042086cc700b5479a8
João Victor Rodrigues Ramalho <joao.ramalho@ee.ufcg.edu.br>
2020-11-19 10:38:23 -0300

    Projeto em branco

---
+++ Projeto_LASD/Projeto_LASD/main.c
@@ -0,0 +1,19 @@
+/*
+ * Projeto_LASD.c
+ *
+ * Created: 19/11/2020 10:30:13
+ * Autores: João Victor Rodrigues Ramalho
+			Matheus Cavalcante Rique 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
